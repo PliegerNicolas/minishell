7146ebd4d7b1089d1d4f7b3c75fadce430620cc8
PliegerNicolas <plieger05@gmail.com>
2023-03-09 09:04:10 +0100

    Not sure if safe.

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nplieger <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 11:24:12 by nplieger          #+#    #+#             */
-/*   Updated: 2023/03/09 08:44:06 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/03/09 08:58:55 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -21,6 +21,8 @@ void	prompt(void)
 	{
 		prompt_prefix();
 		line = readline(STDIN);
+		if (!line)
+			break ;
 		ft_putendl_fd(line, 1);
 		free(line);
 	}
@@ -29,18 +31,13 @@ void	prompt(void)
 int	main(int argc, char **argv, char **env)
 {
 	char				**envp;
-	struct sigaction	sa;
 
 	if (argc != 1)
 		return (1);
 	envp = initialize_env(argc, argv, env);
 	if (!envp)
 		return (1);
-	sa.sa_handler = sighandler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa, NULL);
-	//signal(SIGINT, sighandler);
+	signal(SIGINT, sighandler);
 	prompt();
 	return (free_envp(envp), 0);
 }
--- srcs/signals/sig_handlers.c
+++ srcs/signals/sig_handlers.c
@@ -6,7 +6,7 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 23:12:52 by nicolas           #+#    #+#             */
-/*   Updated: 2023/03/08 23:40:22 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/03/09 09:02:49 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -23,5 +23,8 @@ void	sighandler(int sig)
 	{
 		g_status = failure;
 		ft_putchar_fd('\n', 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		prompt_prefix();
 	}
 }
