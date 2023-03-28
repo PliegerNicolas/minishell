f91f1eb56821d0e0c3ee59196b25987fadfa24e6
PliegerNicolas <plieger05@gmail.com>
2023-03-28 02:47:15 +0200

    If no argument needed, return null, not an empty char **.

--- srcs/parsing/get_arguments.c
+++ srcs/parsing/get_arguments.c
@@ -6,26 +6,28 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 19:46:39 by nicolas           #+#    #+#             */
-/*   Updated: 2023/03/28 01:43:03 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/03/28 02:46:53 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
 
-static char	**new_str_arr(const char **cmds)
+static t_bool	new_str_arr(const char **cmds, char ***args)
 {
 	char	**new_str_arr;
 	size_t	len;
 
 	if (!cmds)
-		return (NULL);
+		return (FALSE);
 	len = 0;
 	while (cmds[len])
 		len++;
+	if (!len)
+		return (*args = NULL, FALSE);
 	new_str_arr = ft_calloc((len + 1), sizeof(*new_str_arr));
 	if (!new_str_arr)
 		return (perror_malloc("@new_str_arr (srcs/parsing/get_arguments.c #new_\
-str_arr"), NULL);
-	return (new_str_arr);
+str_arr"), TRUE);
+	return (*args = new_str_arr, FALSE);
 }
 
 t_bool	get_arguments(const char **cmds, char ***arguments)
@@ -36,9 +38,8 @@ t_bool	get_arguments(const char **cmds, char ***arguments)
 		return (FALSE);
 	else if (arguments && *arguments)
 		return (FALSE);
-	*arguments = new_str_arr(cmds);
-	if (!*arguments)
-		return (*arguments = NULL, TRUE);
+	if (!new_str_arr(cmds, arguments))
+		return (FALSE);
 	i = 0;
 	while (cmds[i])
 	{
