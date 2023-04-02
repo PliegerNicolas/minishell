d8bf6d4e158cd89b4751ffb8ecec46ab58dab174
PliegerNicolas <plieger05@gmail.com>
2023-04-02 17:18:21 +0200

    Adding temporary function to print commands (for testing and comprehension).

--- srcs/execution/executer.c
+++ srcs/execution/executer.c
@@ -6,10 +6,49 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 00:02:51 by nicolas           #+#    #+#             */
-/*   Updated: 2023/03/29 16:41:58 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/04/02 17:17:20 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
+
+// temp function for testing.
+static void	put_commands(t_commands *commands)
+{
+	t_lexer	*lexer;
+	size_t	i;
+	size_t	j;
+
+	if (!commands)
+		return ;
+	i = 1;
+	while (commands)
+	{
+		printf("%s=== Command nº%ld ===%s\n", CYAN, i++, WHITE);
+		printf("commands->cmd : %s\n", commands->cmd);
+		lexer = commands->lexer;
+		printf("%s[Lexer/%s\n", CYAN, WHITE);
+		if (lexer->cmd)
+			printf("lexer->cmd : %s\n", lexer->cmd);
+		if (lexer->exec)
+			printf("lexer->exec : %s\n", lexer->exec);
+		if (lexer->options)
+			printf("lexer->options : %s\n", lexer->options);
+		printf("lexer->pipefds[0] : %d\n", lexer->pipefds[0]);
+		printf("lexer->pipefds[1] : %d\n", lexer->pipefds[1]);
+		if (lexer->args)
+		{
+			j = 0;
+			while (lexer->args[j])
+			{
+				printf("lexer->args[%ld] : %s\n", j, lexer->args[j]);
+				j++;
+			}
+		}
+		printf("%s/Lexer]%s\n", CYAN, WHITE);
+		commands = commands->next;
+		printf("%s=== === ==== === ===%s\n\n", CYAN, WHITE);
+	}
+}
 
 enum e_status	executer(char **envp, char *line)
 {
@@ -22,16 +61,8 @@ enum e_status	executer(char **envp, char *line)
 	commands = parse_user_input(line);
 	if (!commands)
 		return (general_failure);
-	// execute commands
 	// temp
-	if (commands->lexer->exec)
-		printf("%s\n", commands->lexer->exec);
-	if (commands->lexer->options)
-		printf("%s\n", commands->lexer->options);
-	int	i;
-	i = 0;
-	while (commands->lexer->args && commands->lexer->args[i])
-		printf("%s\n", commands->lexer->args[i++]);
+	put_commands(commands);
 	// temp
 	return (free_commands(commands), success);
 }
