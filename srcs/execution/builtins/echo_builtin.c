f01bb7529e504a9ca1f009f4d89e28237b8c7618
Laurent LIAO <laliao@e3r12p23.clusters.42paris.fr>
2023-04-25 21:56:07 +0200

    Order of arguments in array changed only for echo.

--- srcs/execution/builtins/echo_builtin.c
+++ srcs/execution/builtins/echo_builtin.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   echo_builtin.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: laliao <laliao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 18:46:41 by nicolas           #+#    #+#             */
-/*   Updated: 2023/04/19 16:17:00 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/04/25 21:49:47 by laliao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "minishell.h"
 
 static char	*get_quoteless_str(const char *str)
@@ -27,13 +28,67 @@ ltin.c #get_quoteless_str)"), NULL);
 	return (quoteless_str);
 }
 
+char	**get_array_echo(t_lexer *lexer, char **split, char **new_args)
+{
+	char	*tmp_split;
+	size_t	i;
+	size_t	j;
+	size_t	new;
+
+	j = 0;
+	i = 0;
+	new = 0;
+	while (split[i])
+	{
+		tmp_split = get_quoteless_str(split[i]);
+		if (!lexer->args[j])
+		{
+			i++;
+			j = 0;
+		}
+		else if (split[i][0] == '-' && ft_strlen(split[i]) > 2)
+		{
+			new_args[new++] = ft_strdup(split[i]);
+			i++;
+		}
+		else if (strcmp(tmp_split, lexer->args[j]) == 0)
+		{
+			new_args[new++] = ft_strdup(lexer->args[j]);
+			i++;
+			j = 0;
+		}
+		free(tmp_split);
+		j++;
+	}
+	return (new_args);
+}
+
+char	**reparse_echo(t_lexer *lexer)
+{
+	char	**new_args;
+	char	**split;
+	size_t	i;
+
+	i = 0;
+	split = ft_setsplit_quotesafe(lexer->cmd, WHITE_SPACES, none);
+	if (!split)
+		return (free_lexer(lexer), NULL);
+	while (lexer->args[i])
+		i++;
+	new_args = calloc(i + 1, sizeof(char *));
+	new_args = get_array_echo(lexer, split, new_args);
+	free_str_arr(split);
+	free_str_arr(lexer->args);
+	return (new_args);
+}
+
 static t_bool	put(t_lexer *lexer, t_bool n_option, size_t i)
 {
 	char	*quoteless_str;
 
 	while (lexer->args[i])
 	{
-		quoteless_str = get_quoteless_str(lexer->args[i++]);
+		quoteless_str = ft_strdup(lexer->args[i++]);
 		if (!quoteless_str)
 			return (TRUE);
 		ft_putstr_fd(quoteless_str, STDOUT);
@@ -51,17 +106,16 @@ static t_bool	put(t_lexer *lexer, t_bool n_option, size_t i)
 /* Problème avec l'impression des options dû à la stratégie de parsing. */
 t_bool	echo_builtin(t_lexer *lexer)
 {
-	char	*quoteless_str;
 	t_bool	n_option;
 	size_t	i;
 
 	if (!lexer)
 		return (FALSE);
-	quoteless_str = get_quoteless_str(lexer->cmd);
-	if (!quoteless_str)
-		return (g_status = general_failure, TRUE);
 	n_option = FALSE;
 	i = 1;
+	lexer->args = reparse_echo(lexer);
+	if (!lexer->args)
+		return (FALSE);
 	if (lexer->args[i] && strncmp(lexer->args[i], "-n", 2) == 0)
 	{
 		n_option = TRUE;
