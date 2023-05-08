43e6c3c7b3a3fddfd0a83445b0368c1a1bde4c24
PliegerNicolas <plieger05@gmail.com>
2023-05-08 15:09:51 +0200

    norm.

--- srcs/execution/builtins/exit_builtin.c
+++ srcs/execution/builtins/exit_builtin.c
@@ -6,7 +6,7 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 23:49:38 by nicolas           #+#    #+#             */
-/*   Updated: 2023/05/08 15:06:24 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/05/08 15:09:30 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -40,12 +40,11 @@ t_bool	exit_builtin(t_commands *commands, t_lexer *lexer, char ***envp)
 
 	ft_putendl_fd("exit", STDOUT);
 	len = ft_strarrlen((const char **)lexer->args);
-	if (len <= 1)
-		clear_and_exit(commands, envp, NULL);
-	else
+	if (len > 1)
 	{
 		exit_status = ft_atolli(lexer->args[1]);
-		if (!is_num(lexer->args[1]) || exit_status < MIN_INT || exit_status > MAX_INT)
+		if (!is_num(lexer->args[1])
+			|| exit_status < MIN_INT || exit_status > MAX_INT)
 		{
 			errno = EINVAL;
 			perror("exit");
@@ -54,8 +53,7 @@ t_bool	exit_builtin(t_commands *commands, t_lexer *lexer, char ***envp)
 		else if (len > 2)
 		{
 			errno = E2BIG;
-			perror("exit");
-			return (g_status = misuse_of_shell_builtins, FALSE);
+			return (perror("exit"), g_status = misuse_of_shell_builtins, FALSE);
 		}
 		else
 			clear_and_exit(commands, envp, (int *)&exit_status);
