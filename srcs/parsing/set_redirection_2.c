b2e753095885fafb67e84dcbbb3f4c184ea14915
PliegerNicolas <plieger05@gmail.com>
2023-05-11 16:30:55 +0200

    Create files on chained rediriections.

--- srcs/parsing/set_redirection_2.c
+++ srcs/parsing/set_redirection_2.c
@@ -6,17 +6,28 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 01:49:13 by nicolas           #+#    #+#             */
-/*   Updated: 2023/04/24 15:05:55 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/05/11 16:29:33 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
 
 t_bool	set_redir_path(char *pathname, t_lexer *lexer, int slot)
 {
+	int	fd;
+
 	if (!pathname)
 		return (TRUE);
 	if (lexer->redir_path[slot])
+	{
+		if (access(lexer->redir_path[slot], F_OK) == 0
+			&& access(lexer->redir_path[slot], W_OK) == -1)
+			return (perror("access"), TRUE);
+		fd = open_file(lexer->redir_path[slot], lexer->redir_type[slot]);
+		if (fd == -1)
+			return (perror("open"), TRUE);
+		close(fd);
 		free(lexer->redir_path[slot]);
+	}
 	lexer->redir_path[slot] = pathname;
 	return (FALSE);
 }
