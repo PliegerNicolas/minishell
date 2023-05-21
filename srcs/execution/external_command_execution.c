37589896225d1483f9bee684ab7e8c68c8ca0db4
PliegerNicolas <plieger05@gmail.com>
2023-05-21 02:01:22 +0200

    Norm

--- srcs/execution/external_command_execution.c
+++ srcs/execution/external_command_execution.c
@@ -6,7 +6,7 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 20:34:41 by nicolas           #+#    #+#             */
-/*   Updated: 2023/05/21 01:58:49 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/05/21 02:01:13 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -59,7 +59,8 @@ static t_bool	child(t_lexer *lexer, int *pipefds, int *prev_fd, char ***envp)
 {
 	close(pipefds[0]);
 	if (!lexer->exec || !*lexer->exec)
-		return (close_fds(pipefds, prev_fd, TRUE), g_status = command_not_found, TRUE);
+		return (close_fds(pipefds, prev_fd, TRUE),
+			g_status = command_not_found, TRUE);
 	if (infile_redirection(lexer, pipefds, prev_fd))
 		return (close_fds(pipefds, prev_fd, TRUE), g_status = general_failure,
 			TRUE);
