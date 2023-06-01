9b3821b3b67260cbc1343d4e1dc2fa9e020aa094
PliegerNicolas <plieger05@gmail.com>
2023-06-01 13:57:56 +0200

    Corrected some leaks : if the file or directory "ls" exists, I don't know how to treat that case.

--- srcs/parsing/set_exec.c
+++ srcs/parsing/set_exec.c
@@ -6,11 +6,12 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 16:39:19 by nicolas           #+#    #+#             */
-/*   Updated: 2023/06/01 13:37:13 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/06/01 13:56:24 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
 
+// Verifier si le fichier/dossier existe localement ?
 t_bool	set_exec(char *cmd, t_lexer *lexer, char ***envp)
 {
 	if (!lexer)
@@ -18,40 +19,8 @@ t_bool	set_exec(char *cmd, t_lexer *lexer, char ***envp)
 	if (!cmd)
 		return (TRUE);
 	lexer->exec = cmd;
-	if (access(lexer->exec, X_OK) == 0)
-		return (FALSE);
 	lexer->exec = get_path(lexer->exec, envp);
 	if (!lexer->exec)
 		return (TRUE);
 	return (FALSE);
 }
-
-/*
-t_bool	set_exec(const char *str, t_lexer *lexer, t_bool *prev_is_redir,
-	char ***envp)
-{
-	char	*quoteless_str;
-
-	if (!str)
-		return (FALSE);
-	quoteless_str = get_quoteless_str(str);
-	if (!quoteless_str)
-		return (TRUE);
-	if (*quoteless_str == '-' || *quoteless_str == '<' || *quoteless_str == '>')
-	{
-		lexer->exec = ft_strdup("");
-		if (!lexer->exec)
-			return (TRUE);
-		if (*quoteless_str == '-')
-			if (set_options(str, lexer))
-				return (free(quoteless_str), TRUE);
-		if (set_redirection(str, lexer, prev_is_redir, envp))
-			return (free(quoteless_str), TRUE);
-		return (free(quoteless_str), FALSE);
-	}
-	lexer->exec = get_path(quoteless_str, envp);
-	if (!lexer->exec)
-		return (TRUE);
-	return (FALSE);
-}
-*/
--- srcs/utils/ft_only_whitespace.c
+++ srcs/utils/ft_only_whitespace.c
@@ -6,23 +6,18 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 15:48:09 by nicolas           #+#    #+#             */
-/*   Updated: 2023/06/01 13:40:52 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/06/01 13:47:44 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
 
 t_bool	ft_only_whitespace(const char *str)
 {
-	char	*quoteless_str;
-
-	quoteless_str = get_quoteless_str(str);
-	if (!quoteless_str)
-		return (perror_malloc("test"), TRUE);
-	while (*quoteless_str)
+	while (*str)
 	{
-		if (!ft_isspace(*quoteless_str))
+		if (!ft_isspace(*str))
 			return (FALSE);
-		quoteless_str++;
+		str++;
 	}
 	return (TRUE);
 }
