b8502d86ff675cd5b0869758b13e31d1ca48f333
PliegerNicolas <plieger05@gmail.com>
2023-06-02 11:16:57 +0200

    Ignore given arguments. But no error message.

--- srcs/execution/builtins/pwd_builtin.c
+++ srcs/execution/builtins/pwd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 18:59:52 by nicolas           #+#    #+#             */
-/*   Updated: 2023/05/09 23:32:37 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/06/02 11:15:57 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -14,38 +14,12 @@
 t_bool	pwd_builtin(t_lexer *lexer)
 {
 	char	cwd[1024];
-	size_t	len;
 
 	if (!lexer)
 		return (FALSE);
-	len = ft_strarrlen((const char **)lexer->args);
-	if (len > 1)
-	{
-		errno = E2BIG;
-		return (perror("pwd"), g_status = misuse_of_shell_builtins, FALSE);
-	}
 	if (getcwd(cwd, sizeof(cwd)))
 		ft_putendl_fd(cwd, STDOUT);
 	else
 		return (g_status = general_failure, TRUE);
 	return (g_status = success, FALSE);
 }
-/*
-t_bool	pwd_builtin(t_lexer *lexer)
-{
-	char	cwd[1024];
-
-	if (!lexer)
-		return (FALSE);
-	if (ft_strarrlen((const char **)lexer->args) > 1)
-	{
-		errno = E2BIG;
-		return (perror("pwd"), g_status = misuse_of_shell_builtins, FALSE);
-	}
-	if (getcwd(cwd, sizeof(cwd)))
-		ft_putendl_fd(cwd, STDOUT);
-	else
-		return (g_status = command_invoked_cannot_execute, TRUE);
-	return (g_status = success, FALSE);
-}
-*/
